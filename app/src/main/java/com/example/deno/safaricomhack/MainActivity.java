e1821e16faa2c20802d238f2b7dfece1d061be4c
Denis Muthomi <muthomidenis62@gmail.com>
2018-11-09 16:22:24 +0300

    first commit

---
+++ app/src/main/java/com/example/deno/safaricomhack/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.deno.safaricomhack;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.R;
+
+
+  /*This is my innovation app created after safaricom app challenge
+     The app helps Toyota customers to access services more efficiently
+   */
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
